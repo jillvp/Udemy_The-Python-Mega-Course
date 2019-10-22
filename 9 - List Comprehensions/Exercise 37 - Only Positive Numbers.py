41aa8f2e4bf2859f55fd51ad0ec2f4689268a413
Jill van Put <52222203+jillvp@users.noreply.github.com>
2019-10-22 07:06:39 +0200

    Exercise 37 - Only Positive Numbers.py

---
+++ 9 - List Comprehensions/Exercise 37 - Only Positive Numbers.py
@@ -0,0 +1,6 @@
+# Exercise 37: Only Positive Numbers (E) 
+
+# Define a function that takes as parameter list of numbers and returns the list containing only the numbers that are greater than 0.
+
+def foo(lst):
+    return [i for i in lst if i > 0 ]
